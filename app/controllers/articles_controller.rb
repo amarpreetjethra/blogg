b88f5542be11b0d396dfe1178010278bd5660929
Amarpreet Jethra <amyjethra@gmail.com>
2015-06-10 18:43:57 +0530

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ActiveModel::MassAssignmentSecurity
+	attr_accessible :body, :title
 	include ArticlesHelper
 	def show
 		@article = Article.find(params[:id])
@@ -6,30 +8,39 @@ class ArticlesController < ApplicationController
     def find
     end
     def new
-    	@article = Article.new
+@article = Article.new
 
-	end
+end
 	def create
-		  @article = Article.new
-		  @article.title = params[:article][:title]
-		  @article.body = params[:article][:body]
-          @article.save
-          redirect_to article_path(@article)
+  @article = Article.new
+  @article.title = params[:article][:title]
+  @article.body = params[:article][:body]
+  @article.save
+  flash.notice = "Article '#{@article.title}' Created!"
+  redirect_to article_path(@article)
 
-	end
+end
+	
 	def destroy
 		  @article = Article.find(params[:id])
           @article.destroy
+          flash.notice = "Article '#{@article.title}' Destroyed!"
           redirect_to articles_path
 
  	end
  	def edit
-          @article = Article.find(params[:id])
+  @article = Article.find(params[:id])
+end
+   def update
+    @article = Article.find params[:id]
+    if @article.update_attributes(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
+    else
+      render :edit
     end
-    def update
-          @article = Article.find(params[:id])
-  		  @article.update(article_params)
-
-  		  redirect_to article_path(@article)
-	end
+  end
+  
+  
+  
 end
